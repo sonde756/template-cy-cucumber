f6741ab06d22f739c2094629576682cb04285023
fabioaraujoqa <151807639+fabioaraujoqa@users.noreply.github.com>
2024-05-26 23:37:39 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,17 +4,14 @@ const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify")
 
 async function setupNodeEvents(on, config) {
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
-
   on("file:preprocessor", browserify.default(config));
-
-  // Make sure to return the config object as it might have been modified by the plugin.
   return config;
 }
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
-    supportFile: false,
+    supportFile: 'cypress/support/e2e.js',
     setupNodeEvents,
   },
 });
